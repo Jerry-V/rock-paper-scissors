c368038a2c4b1f4f4cf2f7fd882ab0c34afb3626
Jeremiah <jeremiah.vaskis@gmail.com>
2022-04-07 13:01:26 -0600

    Seperate previous functions for new function
    Made a seperate function for returning the win condition message.
    Now 'playRound' only returns the round result and 'returnMessage' the
    appropriate message for that result. This was needed to simply the
    implementation of the 'game()' function.
    "Each function should only return one thing/ do one thing, bla bla"
                                    - some person sometime

--- javascript.js
+++ javascript.js
@@ -17,24 +17,70 @@ function computerPlay(){
 }
 
 // function plays one round of RPS, user inputs should be case insensitive
+// function returns 0 for tie, 1 for win, 2 for lose
 function playRound (playerSelection, computerSelection) {
     // make inputs lowercase
     let userChoice = playerSelection.toLowerCase();
     let compChoice = computerSelection.toLowerCase();
     
+
+    if (userChoice === compChoice) {
+        return 0;
+    } else if (
+        (userChoice === 'rock' && compChoice === 'scissors')
+    ||  (userChoice === 'paper' && compChoice === 'rock')
+    ||  (userChoice === 'scissors' && compChoice === 'paper')) {
+        return 1;
+    } else {
+        return 2;
+    }
+}
+
+// function returns message appropriate to round results
     // compare user choice to computer choice
     // if they tie return tie message
     // if they win return vicotory message
     // if they lose return defeat message
-    if (userChoice === compChoice) {
-        console.log(`You Tie, your ${playerSelection} ties ${computerSelection}`);
-    } else if (userChoice === 'rock' && compChoice === 'scissors') {
-        console.log(`You Win, your ${playerSelection} beats ${computerSelection}`);
-    } else if (userChoice === 'paper' && compChoice === 'rock') {
-        console.log(`You Win, your ${playerSelection} beats ${computerSelection}`);
-    } else if (userChoice === 'scissors' && compChoice === 'paper') {
-        console.log(`You Win, your ${playerSelection} beats ${computerSelection}`);
-    } else {
-        console.log(`You Lose, their ${computerSelection} beats ${playerSelection}`)
+function returnMessage(roundResult, playerSelection, computerSelection){
+        if (roundResult === 0){
+            console.log(`You Tie, you both chose ${playerSelection}`);
+        } else if (roundResult === 1){
+            console.log(`You Win, your ${playerSelection} beats ${computerSelection}`);
+        } else if (roundResult === 2){
+            console.log(`You Lose, their ${computerSelection} beats ${playerSelection}`);
+        } else {
+            console.log(`Something went wrong`);
+        }
+}
+
+// function play 5 rounds, report each round, keep score, report best of 5 winner
+
+// loop process 5 times
+//  for each loop:
+//      ask input, generate computer choice
+//      pass values and determine victor
+//      update score
+//      display round result
+//  at end report overall winner from score value
+function game(){
+    
+    let userChoice;
+    let compChoice;
+    let roundResult;
+    let score=0;
+    
+    for (let i=0; i<5; i++){
+        userChoice = prompt('Enter either Rock, Paper, or Scissors.');
+        compChoice = computerPlay();
+        roundResult = playRound(userChoice,compChoice);
+        returnMessage(roundResult,userChoice,compChoice);
+        if (roundResult === 1){
+            score++;
+        }
+    }
+    
+    if (score >= 3){
+        
     }
 }
+
