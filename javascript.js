6dfdfe8bc8170eebfac5159a7c1039a28fe3ea33
Jeremiah <jeremiah.vaskis@gmail.com>
2022-04-07 10:47:21 -0600

    Create computerPlay function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("hello everyone!")
+console.log("hello everyone!");
+
+// function randonly returns ‘Rock’, ‘Paper’ or ‘Scissors’
+function computerPlay(){
+    // initialize variable 'rand' to 0, 1, or 2 randomly
+    let rand = Math.floor(Math.random()*3);
+    
+    // return ‘Rock’, ‘Paper’ or ‘Scissors’ based on 'rand' value
+    if (rand === 0){
+        return "Rock";
+    } else if (rand === 1){
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
