aaf09e3e79df10796e105f4d46f7276546d13cd5
Jeremiah <jeremiah.vaskis@gmail.com>
2022-04-07 11:24:41 -0600

    Create playRound function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// Rock Paper Scissors (RPS)
 console.log("hello everyone!");
 
 // function randonly returns ‘Rock’, ‘Paper’ or ‘Scissors’
@@ -12,5 +13,28 @@ function computerPlay(){
         return "Paper";
     } else {
         return "Scissors";
+    }
+}
+
+// function plays one round of RPS, user inputs should be case insensitive
+function playRound (playerSelection, computerSelection) {
+    // make inputs lowercase
+    let userChoice = playerSelection.toLowerCase();
+    let compChoice = computerSelection.toLowerCase();
+    
+    // compare user choice to computer choice
+    // if they tie return tie message
+    // if they win return vicotory message
+    // if they lose return defeat message
+    if (userChoice === compChoice) {
+        console.log(`You Tie, your ${playerSelection} ties ${computerSelection}`);
+    } else if (userChoice === 'rock' && compChoice === 'scissors') {
+        console.log(`You Win, your ${playerSelection} beats ${computerSelection}`);
+    } else if (userChoice === 'paper' && compChoice === 'rock') {
+        console.log(`You Win, your ${playerSelection} beats ${computerSelection}`);
+    } else if (userChoice === 'scissors' && compChoice === 'paper') {
+        console.log(`You Win, your ${playerSelection} beats ${computerSelection}`);
+    } else {
+        console.log(`You Lose, their ${computerSelection} beats ${playerSelection}`)
     }
 }
